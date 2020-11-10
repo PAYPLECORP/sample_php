5724b371436fda73e0c79f67a1a5041de87b11cf
payple <hc.kim@payple.kr>
2020-11-10 17:48:41 +0900

    Update payple_payAuth.php

--- pg/pay/cPayPayple/payple_payAuth.php
+++ pg/pay/cPayPayple/payple_payAuth.php
@@ -1,42 +1,50 @@
-    /* 
-    * TEST : https://testcpay.payple.kr/php/auth.php
-    * REAL : https://cpay.payple.kr/php/auth.php 
-    */
-    header("Expires: Mon 26 Jul 1997 05:00:00 GMT");
-    header("Last-Modified: " . gmdate("D, d, M Y H:i:s") . " GMT");
-    header("Cache-Control: no-store, no-cache, must-revalidate");
-    header("Cache-Control: post-check=0; pre-check=0", false);
-    header("Pragma: no-cache");
-    header("Content-type: application/json; charset=utf-8");
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/payple/inc/config.inc';
+header("Expires: Mon 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d, M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0; pre-check=0", false);
+header("Pragma: no-cache");
+header("Content-type: application/json; charset=utf-8");
 
-    //API를 받는 서버와 결제창 연동 서버가 다른 경우 또는 AWS 이용 가맹점인 경우 REFERER 에 도메인을 넣어주세요.
-    $CURLOPT_HTTPHEADER = array(
-        "referer: http://localhost:80"
-    );
+/*
+ * payple_dir_path : cPayPayple 폴더 경로
+ * ORDER PAGE(ex: order_confirm.html) 의 CpayForm 에  payple_dir_path 값을 지정하거나
+ * 여기서 직접 지정할 수 있습니다.
+ */
+$payple_dir_path = (isset($_POST['payple_dir_path'])) ? $_POST['payple_dir_path'] : "";
+//$payple_dir_path = "/pg/pay";
 
-    // 발급받은 비밀키. 유출에 주의하시기 바랍니다.
-    // 실제 서비스에 붙이실 때는 발급받은 운영 계정 키를 넣어주세요.
-    $post_data = array (
-        "cst_id" => "test",
-        "custKey" => "abcd1234567890"
-    );
+// 발급받은 비밀키. 유출에 주의하시기 바랍니다.
+// payple_dir_path : cPayPayple 설치 경로
+$post_data = array (
+	"cst_id" => $cst_id,
+    "custKey" => $custKey
+);
+
+$POSTFIELDS = json_encode($post_data);
     
-    $ch = curl_init('https://testcpay.payple.kr/php/auth.php');
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSLVERSION, 4);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $CURLOPT_HTTPHEADER);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+$CURLOPT_HTTPHEADER = array(
+		"cache-control: no-cache",
+		"content-type: application/json; charset=UTF-8"
+);
 
-    ob_start();
-    $response = curl_exec ($ch);
-    $buffer = ob_get_contents();
-    ob_end_clean();
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSLVERSION, 4);
+curl_setopt($ch, CURLOPT_REFERER, $SERVER_NAME);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $CURLOPT_HTTPHEADER);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $POSTFIELDS);
 
-    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    
-    curl_close ($ch);
-    if($status_code == 200) {
-        echo $buffer;
-    }
+ob_start();
+$res = curl_exec($ch);
+$buffer = ob_get_contents();
+ob_end_clean();
 
+if (!$buffer) {
+    $returnVal = "";
+} else {
+    echo $buffer;
+}
+?>
